947d7fdda7a471f4cc5b9d98a91a4a613d7d7e97
lmy <1429418347@qq.com>
2022-11-26 09:16:02 +0800

    '剩余echarts'

--- js/index.js
+++ js/index.js
@@ -18,11 +18,11 @@ for (var i = 0; i < fault1.length; i++) {
 // 右侧最上方时间
 var ul_one_lis = document.getElementsByClassName('ul_one')[0].children;
 var ul_data = document.getElementsByClassName('ul_data');
-
+var index_ = 0;
 for (var i = 0; i < ul_one_lis.length; i++) {
     ul_one_lis[i].setAttribute('index', i);
     ul_one_lis[i].onclick = function () {
-        var index_ = this.getAttribute('index')
+        index_ = this.getAttribute('index')
         for (var j = 0; j < ul_one_lis.length; j++) {
             ul_one_lis[j].classList.remove('active');
             ul_one_lis[index_].classList.add('active');
@@ -35,22 +35,54 @@ for (var i = 0; i < ul_one_lis.length; i++) {
     }
 
 }
-var ind = 0;
-var timer;
-function timeFn() {
+let timer;
+function time() {
     timer = setInterval(function () {
-        ind++;
-        if (ind >= ul_one_lis.length) {
-            ind = 0;
+        index_++;
+        if (index_ >= ul_one_lis.length) {
+            index_ = 0;
         }
-        ul_one_lis[ind].click()
+        ul_one_lis[index_].click()
     }, 1500)
 }
-timeFn()
-var day = document.getElementsByClassName('day')[0];
-day.onmouseenter = function () {
+time()
+var day_ = document.getElementsByClassName('day')[0];
+day_.onmouseenter = function () {
     clearInterval(timer)
 }
-day.onmouseleave = function () {
-    timeFn()
+day_.onmouseleave = function () {
+    time()
+}
+// 销售额统计
+
+var sales_ul = document.getElementsByClassName('sales')[0].getElementsByTagName('ul')[0];
+var sales_lis = sales_ul.getElementsByTagName('li');
+var ind = 0;
+var timer1;
+for (var i = 0; i < sales_lis.length; i++) {
+    sales_lis[i].setAttribute('index', i);
+    sales_lis[i].onclick = function () {
+        ind = this.getAttribute('index');
+        for (var j = 0; j < sales_lis.length; j++) {
+            sales_lis[j].classList.remove('active');
+            sales_lis[ind].classList.add('active')
+        }
+    }
+}
+function time1() {
+    timer1 = setInterval(function () {
+        ind++;
+        if (ind >= sales_lis.length) {
+            ind = 0;
+        }
+        sales_lis[ind].click()
+    }, 1800)
+}
+time1()
+var sales_ = document.getElementsByClassName('sales')[0];
+sales_.onmouseenter = function () {
+    clearInterval(timer1)
+}
+sales_.onmouseleave = function () {
+    time1()
 }
